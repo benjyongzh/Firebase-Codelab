588b94cd8a39ad7830925ddb1265a6dd85cfaa2e
Benjamin Yong <benj_yongzh@hotmail.com>
2023-03-01 22:22:25 +0800

    Notifications.

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,1 +1,8 @@
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
+import { getFirebaseConfig } from "./firebase-config";
+
 // TODO: Set up Firebase Cloud Messaging service worker
+const firebaseApp = initializeApp(getFirebaseConfig());
+getMessaging(firebaseApp);
+console.info('Firebase messaging service worker is set up');
